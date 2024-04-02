b9d052c31600458092419491cd24b42b5dc5ca81
DzuNa <dzulfidho76@gmail.com>
2024-04-03 06:26:18 +0700

    Bab 3 Rev

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// Carousel
+//Carousel automatically
 var firstindex = 0;
 function autoSlide() {
   var gbr;
@@ -31,3 +31,37 @@ function autoSlide() {
   setTimeout(autoSlide, 5000);
 }
 autoSlide();
+
+//Carousel Button
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("imgslide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
